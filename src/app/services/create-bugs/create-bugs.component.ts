60fb57a9e6370ebbe1f1a65dfb678998634d962c
Bouris <diamantis.bouris@accenture.com>
2019-12-20 16:16:00 +0200

    fixed cooments formarray

--- src/app/services/create-bugs/create-bugs.component.ts
+++ src/app/services/create-bugs/create-bugs.component.ts
@@ -3,7 +3,6 @@ import { GetBugsService } from "../get-bugs.service";
 import { tap } from "rxjs/operators";
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
-
 @Component({
   selector: "bug-reporting-system-create-bugs",
   templateUrl: "./create-bugs.component.html",
@@ -13,18 +12,14 @@ export class CreateBugsComponent implements OnInit {
   form: FormGroup
   commentForm: FormGroup
   bugId: string
-
   constructor(private createBugs: GetBugsService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private formBuilder: FormBuilder) { }
-
   ngOnInit() {
     this.bugId = this.activatedRoute.snapshot.params["id"]
-    this.getBug(this.bugId)
     this.initializeFormState();    
   }
-
   initializeFormState() {
     this.form = this.formBuilder.group(
       {
@@ -35,48 +30,43 @@ export class CreateBugsComponent implements OnInit {
         status: ['', Validators.required],        
         comments: this.formBuilder.array([this.getcomments()])
       }
-
     )
+    this.getBug(this.bugId)
   }  
-
   submitbug() {
     if (this.form.invalid) {
       return
     }
-
     const actionToInvoke = this.bugId
       ? this.createBugs.editBug(this.bugId, this.form.value)
       : this.createBugs.createBugs(this.form.value)
-
-
     actionToInvoke.pipe(
       tap(() => this.router.navigate([""]))
     ).subscribe()
-
   }
-
   private getBug(id) {
     if (!id) {
       return
     }
     this.createBugs.getBug(id).subscribe(data => {
+      this.commentsArray.removeAt(0); // remove the first bug that have been created in initializeFormState method
+      data.comments.forEach(comment => {
+        this.addComment(comment)
+      })
       this.form.patchValue(data);     
     })
   }
-
-  getcomments() {
+  getcomments(comment = null) {
     return this.formBuilder.group({
-      description: [],
-      reporter: [],
-      id: []
+      description: [comment && comment.description],
+      reporter: [comment && comment.reporter],
+      id: [comment && comment.id]
     });
   }
-
   get commentsArray() {
     return <FormArray>this.form.get('comments');
   }
-
-  addComment() {
-    this.commentsArray.push(this.getcomments());
+  addComment(comment = null) {
+    this.commentsArray.push(this.getcomments(comment));
   }
 }
